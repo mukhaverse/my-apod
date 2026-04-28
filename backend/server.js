bc8ff0732d22b9bb61e1a3d7ddd4743cd06de8d8
Shumokh <shumokhalsharif@gmail.com>
2026-04-28 20:02:22 +0300

    added backend

---
+++ backend/server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+
+
+const app = express();
+app.use(cors());
+
+
+
+
+
+app.get("/apod-image", async (req, res) => {
+
+  const imageUrl = req.query.url;
+  console.log("Fetching:", imageUrl);
+
+
+  if (!imageUrl || !imageUrl.includes("apod.nasa.gov")) {
+    return res.status(400).send("Invalid image source");
+  }
+
+
+
+  try {
+    const response = await fetch(imageUrl);
+
+    if (!response.ok) {
+      console.error("Bad response:", response.status, imageUrl);
+      return res.status(500).send("Image not found or blocked");
+    }
+
+
+    const buffer = await response.arrayBuffer();
+
+
+
+    res.set({
+      "Content-Type": response.headers.get("content-type") || "image/jpeg",
+      "Access-Control-Allow-Origin": "*"
+    });
+
+    res.send(Buffer.from(buffer));
+
+  } catch (err) {
+    console.error("Fetch error:", err);
+    res.status(500).send("Failed to fetch image");
+  }
+
+
+
+});
+
+
+app.get("/", (req, res) => {
+  res.send("APOD proxy running");
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
